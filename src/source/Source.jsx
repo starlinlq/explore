b9b22826ae293f70576b36ae5066b2b7452938fa
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-04-05 23:13:15 -0500

    changed layout source

--- src/source/Source.jsx
+++ src/source/Source.jsx
@@ -8,55 +8,8 @@ const Source = () => {
             <ImgWrapper>
               <Img src={mg} />
             </ImgWrapper>
-            <FormWrapper>
-              <Title>Register now</Title>
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                <Label>Name</Label>
-                {errors.name && <Error>{errors.message}</Error>}
-                <InputWrapper>
-                  <RiContactsFill />
-                  <Input
-                    placeholder="name"
-                    name="name"
-                    ref={register({
-                      required: "true",
-                      maxLength: {
-                        value: 10,
-                        message: "name should be 10 character or less",
-                      },
-                      pattern: /^[A-Za-z]+$/i,
-                    })}
-                  />
-                </InputWrapper>
-                <Label>Email</Label>
-                {errors.email && <Error>name is required</Error>}
-                <InputWrapper>
-                  <HiOutlineMail />
-                  <Input
-                    type="email"
-                    placeholder="email"
-                    name="email"
-                    ref={register({ required: true })}
-                  />
-                </InputWrapper>
-  
-                <Label>Password</Label>
-                <InputWrapper>
-                  <RiLockPasswordLine />
-                  <Input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    ref={register({ required: true })}
-                  />
-                  {errors.password && <Error>name is required</Error>}
-                </InputWrapper>
-                <Button type="submit">Submit</Button>
-              </Form>
-              <Link to="login">
-                <Span>Already have an account, Log in</Span>
-              </Link>
-            </FormWrapper>
+           
+          
           </Section>
         </Container>
       </Wrapper>
